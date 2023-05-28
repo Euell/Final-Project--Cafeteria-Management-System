4af70b3b477463793f1f7376277f4dbdd06ab1f4
Emmanuel Manalo <mr.manaloemmanuel@gmail.com>
2023-05-28 11:53:43 +0800

    added more conditional statements for payment control

--- Final Project- Cafeteria Management System/paymentControl1.cs
+++ Final Project- Cafeteria Management System/paymentControl1.cs
@@ -19,8 +19,17 @@ namespace Final_Project__Cafeteria_Management_System
 
         private void payBtn_Click(object sender, EventArgs e)
         {
-            int price = Convert.ToInt32(totalPriceLbl.Text);
-            int amount = Convert.ToInt32(paymentAmountLbl.Text);
+
+            int price = 0, amount = 0;
+            try
+            {
+                 price = Convert.ToInt32(totalPriceLbl.Text);
+                 amount = Convert.ToInt32(paymentAmountLbl.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please check your inputted details");
+            }
             if (customerName.Text == "")
             {
                 MessageBox.Show("Please input a username");
@@ -42,7 +51,8 @@ namespace Final_Project__Cafeteria_Management_System
             }
             else
             {
-                dataGridView1.Rows.Add(customerName.Text, totalPriceLbl.Text, paymentAmountLbl.Text, totalChange.Text, DateTime.Now.ToString());
+                DateTime now = DateTime.Now;
+                dataGridView1.Rows.Add(customerName.Text, totalPriceLbl.Text, paymentAmountLbl.Text, totalChange.Text, now.ToLongTimeString());
                 //price.Visible = true;
             }
 
